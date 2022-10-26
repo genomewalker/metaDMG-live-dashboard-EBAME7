31b25fdcc18783332599b50d709aaa0801e799af
Christian Michelsen <christianmichelsen@gmail.com>
2022-10-26 16:43:27 +0200

    Add files

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-# poetry add "metaDMG[viz]"@latest
-# poetry export --output requirements.txt --extras viz
-poetry install
-poetry run pip install --upgrade pip
-poetry run pip install metaDMG"[viz]" --upgrade
+poetry add "metaDMG[viz]"@latest
+poetry export --output requirements.txt --extras viz
+# poetry install
+# poetry run pip install --upgrade pip
+# poetry run pip install metaDMG"[viz]" --upgrade
 git add .
 git commit -m 'Add files'
 git push
